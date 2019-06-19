337ffa2c637a82c7be8deb07ba5bec3b5f133b27
esther <estherkc100@gmail.com>
2019-06-19 12:16:48 +0300

    added flask bootstrap

--- list/__init__.py
+++ list/__init__.py
@@ -1,6 +1,7 @@
 from list import views
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 # intializing application
 
 app = Flask(__name__, instance_relative_config=True)
@@ -8,3 +9,6 @@ app = Flask(__name__, instance_relative_config=True)
 # Setting up configurations
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#initializing Flask Extensions
+bootstrap = Bootstrap(app)
--- list/views.py
+++ list/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from .request import get_movies
+from .request import get_movies,get_movie
 from list.__init__ import app
 
 @app.route('/')
@@ -13,3 +13,14 @@ def index():
     print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title=title, popular=popular_movies)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
