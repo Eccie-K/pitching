11d027319c3a8ebc914bdf663acf82c961fca8c2
esther <estherkc100@gmail.com>
2019-06-24 12:42:27 +0300

    added navigation bar

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL =  'https://newsapi.org/v1/sources?={}&apiKey={}'
+    NEWS_API_BASE_URL =  'https://newsapi.org/v2/{}?apiKey={}'
+    NEWS_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
