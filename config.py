f40f0d64541bd74c0c671a7237aab83c1fe069cf
esther <estherkc100@gmail.com>
2019-06-30 09:24:39 +0300

    deleted content in request package

--- config.py
+++ config.py
@@ -2,12 +2,7 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL =  'https://newsapi.org/v2/sources?apiKey=8042f30000994df78898ee8fafe63983'
-    NEWS_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
+    
 
 class ProdConfig(Config):
     pass
